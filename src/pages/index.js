3bf2ba8afdb82ea485f436ac8736c34ee5f3188c
jeriannh <jlhsiao@uwaterloo.ca>
2021-01-30 12:18:35 -0500

    Initial Gatsby starter with Tailwind

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import React from "react"
 
 export default function Home() {
-  return <div className="text-purple-600">Hello world!</div>
+  return <div className="text-red-600">Hello world!</div>
 }
