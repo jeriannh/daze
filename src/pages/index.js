c6c0b2bd9010e861befeb8211ad79a8fc8c15f46
jeriannh <jlhsiao@uwaterloo.ca>
2021-01-30 14:32:10 -0500

    Finished landing page and added fonts and colours

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,22 @@
 import React from "react"
+import { Link } from "gatsby"
+import logo from "../graphics/daze-logo.svg"
 
 export default function Home() {
-  return <div className="text-red-600">Hello world!</div>
+  return (
+    <div className="h-screen flex items-center">
+      <div className="w-full flex flex-col items-center gap-12">
+        <img className="w-96" src={logo} alt="Daze" />
+        <div className="text-3xl text-blue font-pn-bold">
+          Turn your daze into 7 days of inward and outward transformation
+        </div>
+        <Link
+          className="rounded-full bg-orange text-white font-pn-bold text-2xl px-8 py-4 mt-28"
+          to="/goal"
+        >
+          Start planning your week
+        </Link>
+      </div>
+    </div>
+  )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,19 @@
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}'],
+  purge: ["./src/**/*.{js,jsx,ts,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        pink: "#F1BEA8",
+        orange: "#E3966D",
+        blue: "#006371",
+        blueGrey: "#BDCDCC",
+        grey: "#F5F5F5",
+      },
+      fontFamily: {
+        "pn-bold": ["Proxima Nova Bold"],
+      },
+    },
   },
   variants: {
     extend: {},
